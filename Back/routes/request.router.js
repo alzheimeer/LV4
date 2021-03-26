75286d8f95a8430d0c6c634e3f3e58ceae06526f
Edgar Carlos M. Quintero <fogniebla@hotmail.com>
2021-03-26 02:22:37 -0500

    fix

--- Back/routes/request.router.js
+++ Back/routes/request.router.js
@@ -1,15 +1,15 @@
 const { Router } = require('express');
 const router = Router();
-const { createRequest, getRequests, getRequestById, updateRequestById, deleteRequestById} = require('../controllers/request.controller');
+const { createRequest, getRequests, getRequestById, updateRequestById, deleteRequestById } = require('../controllers/request.controller');
 const { validarJWT, isAdmin, isModerator, isUser } = require('../middlewares/validar-jwt');
 
-router.post('/', [validarJWT, isModerator], createRequest);
+router.post('/', [validarJWT, isUser], createRequest);
 
 router.get('/', getRequests);
 
 router.get('/:requestId', getRequestById);
 
-router.put('/:requestId', [validarJWT, isAdmin], updateRequestById);
+router.put('/:requestId', [validarJWT, isUser], updateRequestById);
 
 router.delete('/:requestId', [validarJWT, isAdmin], deleteRequestById);
 
