8ebe6288e084a44e4aa50462b639383912e513cb
Edgar Carlos M. Quintero <fogniebla@hotmail.com>
2021-04-09 11:14:01 -0500

    fix angular.json karma.json

--- Front/karma.conf.js
+++ Front/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function(config) {
             suppressAll: true // removes the duplicated traces
         },
         coverageReporter: {
-            dir: require('path').join(__dirname, './coverage/Lendiapp'),
+            dir: require('path').join(__dirname, './coverage/Lendiup'),
             subdir: '.',
             reporters: [
                 { type: 'html' },
